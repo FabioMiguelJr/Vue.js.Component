bde8ed2ad8a3d6a1e5e82a25da5d58227406451f
FabioMiguelJr <74741157+FabioMiguelJr@users.noreply.github.com>
2022-07-30 18:13:34 -0300

    Commit - Passagem de Slots entre componentes

--- src/components/LocalComponent.vue
+++ src/components/LocalComponent.vue
@@ -3,6 +3,11 @@
   <h2>{{ title }}</h2>
   <button @click="$emit('addclick', 1)">Add</button>
   <button @click="addTwo">Add Two</button>
+  <br />
+  <!-- <slot></slot> -->
+  <slot name="header"></slot>
+  <br />
+  <slot name="content"></slot>
 </template>
 
 <script>
