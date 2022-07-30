a591db57466a8e5765c813423b4925e2ca601df1
FabioMiguelJr <74741157+FabioMiguelJr@users.noreply.github.com>
2022-07-29 23:01:12 -0300

    Componentes Vue.js

---
+++ src/components/LocalComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Local Component</h1>
+  <h2>{{ title }}</h2>
+</template>
+
+<script>
+export default {
+  //props: ["title"],
+  props: {
+    title: String,
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
